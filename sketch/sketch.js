602feab5c4d2eefaea130a6f7ed36aa1076a9f77
Dan Hunter <dan.hunter55@yahoo.com>
2018-10-18 10:14:05 +0100

    Code changes - worksop3

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,11 +1,11 @@
 function setup() {//defines the actions carried out by the setup function
   createCanvas(400, 400)//creates canvas to draw on
-  background(0)//sets backround colour
+  background(255)//sets backround colour
 }
 
 function draw() {//defines the actions carried out by the draw function
-  roof()
-  house()
+background(255)
+  house(mouseX, mouseY)
 }
 //x is an interger type and represents distance(in pixles) from the x axis
 //y is an interger type and represents distance(in pixles) from the y axis
@@ -19,17 +19,12 @@ function square(x, y, size) {
 function roof(x, y) {
   fill(255, 130, 0)//sets the colour of the drawings
   stroke(0)//sets the outline colour of the drawings
-  triangle(0, 30, 40, 0, 80, 30)//defines the perameters of the triangle
-  triangle(75, 30, 115, 0, 155, 30)
-  triangle(150, 30, 190, 0, 230, 30)
+  triangle(x, y, x + 40, y - 30, x + 80, y)//defines the perameters of the triangle
+
 }
 
 function house(x, y) {
-  square(5, 30, 70)//defines the perameters of the rectangle
-  square(80, 30, 70)
-  square(155, 30, 70)
-}
-function house(x, y, height, width) {
-  rect(5, 30, 70, 70)//defines the perameters of the rectangle
- 
+  roof(x, y)
+  square(x + 5, y, 70)//defines the perameters of the rectangle
+
 }
